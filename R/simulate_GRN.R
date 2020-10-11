0ccabcb3536bcfcf43ee9eb38f72c188f0c404be
igbucur <ioan.gabriel.bucur@gmail.com>
2020-10-11 12:44:10 +0200

    added lower and upper limit to simulate_GRN, filled in documentation

--- R/simulate_GRN.R
+++ R/simulate_GRN.R
@@ -6,7 +6,9 @@
 #' @param prob_edge Numeric probability of having a causal interaction (edge)
 #' between two expression traits. Passed on to \link[pcalg]{randomDAG}.
 #' @param seed Integer seed for reproducible random number generation.
-#'
+#' @param lwr_edge_weight Lower limit of edge weight (causal strength T_i -> T_j)
+#' @param upr_edge_weight Upper limit of edge weight (causal strength T_i -> T_j)
+#' 
 #' @return A data set consisting of \emph{nobs} samples from a random generated 
 #' gene regulatory network. This includes \emph{ngen} binomially distributed 
 #' variables representing the genetic markers associated with the genetic makeup 
@@ -16,37 +18,36 @@
 #' @export
 #'
 #' @examples simulate_GRN(10, 10, 100, 0.1, 1634)
-simulate_GRN <- function(ngen, nexp, nobs, prob_edge, seed = NULL) {
+simulate_GRN <- function(ngen, nexp, nobs, prob_edge,  seed = NULL,
+                         lwr_edge_weight = -1, upr_edge_weight = 1) {
   
   set.seed(seed)
   
-  # dense graph
-  G <- pcalg:::randomDAG(nexp, prob_edge, lB = 1, uB = 1, V = paste0("T", 1:nexp))
+  # Generate causal graph expressing causal regulatory relationships
+  G <- pcalg::randomDAG(nexp, prob_edge, V = paste0("T", 1:nexp),
+                        lB = lwr_edge_weight, uB = upr_edge_weight)
   
-  stopifnot(nexp >= ngen)
+  # Generate random minor allele frequencies (MAF)
+  theta <- stats::runif(ngen, 0.1, 0.5) 
   
-  ## Generate data
-  
-  theta <- runif(ngen, 0.1, 0.5) 
-  
-  L <- t(sapply(theta, rbinom, n = nobs, size = 1)) + 1
+  # Generate genetic marker data from a binomial distribution
+  L <- t(sapply(theta, stats::rbinom, n = nobs, size = 1)) + 1
   rownames(L) <- paste0("L", 1:ngen)
-  L.pos <- matrix(1, ngen, 2); L.pos[, 2] <- 1:ngen # one chromosome, each in the same position
+  # we consider a single chromosome, locus identical to row number
+  L.pos <- matrix(1, ngen, 2); L.pos[, 2] <- 1:ngen 
   
-  
-  B <- t(as(G, "matrix")) # print(paste("Number of edges:", sum(B)))
+  B <- t(methods::as(G, "matrix")) 
+  print(paste("Number of edges:", sum(B != 0)))
   C <- matrix(0, nexp, ngen); C[1:ngen, 1:ngen] <- diag(ngen)
   
-  Tr <- solve(diag(nexp) - B) %*% (C %*% L + matrix(rnorm(nexp * nobs), nexp, nobs))
-  # Tr <- t(apply(Tr, 1, function(row) row / sd(row))) # normalize to Var[X] = 1
-  
-  Trait <- Tr
-  # Trait <- t(apply(Tr, 1, function(x) qnorm( rank(x) / (length(x) + 1) ) ) ) # re-normalize data
+  # Generate expression data on nexp traits
+  Trait <- solve(diag(nexp) - B) %*% (C %*% L + matrix(stats::rnorm(nexp * nobs), nexp, nobs))
   T.pos <- matrix(1, nexp, 3); T.pos[, 2:3] <- 1:nexp
   
-  data <- t(rbind(L, Tr))
+  # combine marker and trait data to compute summary statistics
+  data <- t(rbind(L, Trait))
   
   list(L = L, Trait = Trait, L.pos = L.pos, T.pos = T.pos, 
-       cov.matrix = cov(data), means = colMeans(data),
-       cor.matrix = cor(data), B = B, num_edges = sum(B))
+       cov.matrix = stats::cov(data), means = colMeans(data),
+       cor.matrix = stats::cor(data), B = B, num_edges = length(G@edgeData@data))
 }
