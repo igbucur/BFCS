69cb47d68e939184bfa427851bda3585cb2743bb
igbucur <ioan.gabriel.bucur@gmail.com>
2020-10-11 12:45:03 +0200

    standardized functions for inferring GRNs and moved them to one file

--- R/infer_GRN.R
+++ R/infer_GRN.R
@@ -1,4 +1,59 @@
 #' Compute the posterior probabilities of L_k -> T_i -> T_j for a simulated GRN 
+#' using BFCS::compute_Bayes_factors_vectorized.
+#'
+#' @param GRN List produced by simulate_GRN containing GRN structure and statistics.
+#' @param prior Prior on causal graphs (default uniform on DMAGs with bkg. info.)
+#' @param loc_link Integer vector specifying genetic marker with strongest local
+#' linkage for each expression trait. If not specified, we use the default BFCS 
+#' maximum probability search strategy.
+#'
+#' @return A numeric matrix of posterior probabilities for each possible causal
+#' relationship between the expression traits. 
+#' @export
+#'
+#' @examples
+#' GRN <- simulate_GRN(10, 10, 1000, 0.1)
+#' compute_BFCS_probabilities(GRN)
+#' compute_BFCS_probabilities(GRN, loc_link = 10:1)
+compute_BFCS_probabilities <- function(GRN, prior = uniform_prior_GRN_DMAG(), loc_link = NULL) {
+  
+  ngen <- nrow(GRN$L)
+  nexp <- nrow(GRN$Trait)
+  
+  # For fairness in calculating time complexity, we recompute GRN$cor.matrix
+  GRN$cor.matrix <- stats::cor(t(rbind(GRN$L, GRN$Trait)))
+  
+  cLT <- GRN$cor.matrix[1:ngen, (ngen+1):(ngen+nexp)]
+  cTT <- GRN$cor.matrix[(ngen+1):(ngen+nexp), (ngen+1):(ngen+nexp)]
+  
+  Bayes_factors <- lapply(1:nexp, function(i) {
+    compute_Bayes_factors_vectorized(rep(cLT[, i], nexp), c(cLT), rep(cTT[i, ], each = ngen),
+                                     num_samples = ncol(GRN$L))
+  })
+  
+  posterior_probabilities <- sapply(1:nexp, function(i) {
+    Bf <- prior * t(Bayes_factors[[i]])
+    prob <- matrix(Bf[6, ] / colSums(Bf), ngen, nexp)
+    
+    if (is.null(loc_link)) {
+      prob <- apply(prob, 2, max)
+    } else {
+      prob <- prob[loc_link[i], ]
+    }
+
+    prob[i] <- 0
+    prob
+  })
+  
+  dimnames(posterior_probabilities) <- list(
+    rownames(GRN$Trait), rownames(GRN$Trait)
+  )
+  
+  posterior_probabilities
+}
+
+
+#' Compute the posterior probabilities of L_k -> T_i -> T_j for a simulated GRN 
 #' using bfcs::compute_BGe_score_vectorized.
 #'
 #' @param GRN List produced by simulate_GRN containing GRN structure and statistics.
@@ -10,8 +65,8 @@
 #'
 #' @examples
 #' GRN <- simulate_GRN(10, 10, 100, 0.1)
-#' get_BGe_probabilities(GRN)
-get_BGe_probabilities <- function(GRN, prior = uniform_prior_GRN_DMAG()) {
+#' compute_BGe_probabilities(GRN)
+compute_BGe_probabilities <- function(GRN, prior = uniform_prior_GRN_DMAG()) {
   
   ngen <- nrow(GRN$L)
   nexp <- nrow(GRN$Trait)
@@ -19,7 +74,7 @@ get_BGe_probabilities <- function(GRN, prior = uniform_prior_GRN_DMAG()) {
   # Markov equivalence classes for three nodes
   mec_list <- get_Markov_equivalence_class_list()
   
-  bnlearn_sim <- lapply(1:nexp, function(i) {
+  Bayes_factors <- lapply(1:nexp, function(i) {
     my_grid <- expand.grid(
       L_k = 1:ngen,
       T_i = ngen + i,
@@ -33,13 +88,19 @@ get_BGe_probabilities <- function(GRN, prior = uniform_prior_GRN_DMAG()) {
     compute_BGe_score_vectorized(suffstat_list, ncol(GRN$L), mec_list)
   })
   
-  sapply(1:nexp, function(i) {
-    Bf <- apply(bnlearn_sim[[i]], 1, function(lev) exp(lev - max(lev)) * prior)
+  posterior_probabilities <- sapply(1:nexp, function(i) {
+    Bf <- apply(Bayes_factors[[i]], 1, function(lev) exp(lev - max(lev)) * prior)
     prob <- matrix(Bf[6, ] / colSums(Bf), ngen, nexp)
     prob <- apply(prob, 2, max)
     prob[i] <- 0
     prob
   })
+  
+  dimnames(posterior_probabilities) <- list(
+    rownames(GRN$Trait), rownames(GRN$Trait)
+  )
+  
+  posterior_probabilities
 }
 
 #' Compute the posterior probabilities of L_k -> T_i -> T_j for a simulated GRN
@@ -54,13 +115,13 @@ get_BGe_probabilities <- function(GRN, prior = uniform_prior_GRN_DMAG()) {
 #'
 #' @examples
 #' GRN <- simulate_GRN(5, 5, 100, 0.1)
-#' get_BiDAG_probabilities(GRN)
-get_BiDAG_probabilities <- function(GRN, prior = uniform_prior_GRN_DMAG()) {
+#' compute_BiDAG_probabilities(GRN)
+compute_BiDAG_probabilities <- function(GRN, prior = uniform_prior_GRN_DMAG()) {
   
   ngen <- nrow(GRN$L)
   nexp <- nrow(GRN$Trait)
   
-  prob_matrix <- matrix(0, nexp, nexp)
+  posterior_probabilities <- matrix(0, nexp, nexp)
   
   mec_list <- get_Markov_equivalence_class_list(format = "amat")
   
@@ -80,16 +141,60 @@ get_BiDAG_probabilities <- function(GRN, prior = uniform_prior_GRN_DMAG()) {
         
         # Compute posterior probabilities
         lev <- sapply(mec_list, function(mec) {
-          BiDAG::DAGscore(3, BiDAG::scoreparameters(3, "bge", triple), mec)
+          BiDAG::DAGscore(BiDAG::scoreparameters(3, "bge", triple), mec)
         })
         
         ev <- exp(lev - max(lev)) * prior
         ev <- ev / sum(ev) # normalize probabilities
         ev[6] # probability of L_k -> T_i -> T_j
       })
-      prob_matrix[j, i] <- max(probs)
+      posterior_probabilities[j, i] <- max(probs)
     }
   }
   
-  prob_matrix
+  dimnames(posterior_probabilities) <- list(
+    rownames(GRN$Trait), rownames(GRN$Trait)
+  )
+  
+  posterior_probabilities
+}
+
+#' Compute the posterior probabilities of L_k -> T_i -> T_j for a simulated GRN
+#' using the trigger package.
+#'
+#' @param GRN List produced by simulate_GRN containing GRN structure and statistics.
+#' @param trigger_obj Precomputed object of class 'trigger' (build + loclink).
+#' @param seed Random seed for reproducibility.
+#' @param window.size Window size when looking for genetic markers. Default
+#' option is value chosen in Chen et al. (2007)
+#'
+#' @return A numeric matrix of posterior probabilities for each possible causal
+#' relationship between the expression traits. 
+#' @export
+#'
+#' @examples
+#' GRN <- simulate_GRN(100, 100, 100, 0.01)
+#' compute_trigger_probabilities(GRN)
+compute_trigger_probabilities <- function(GRN, trigger_obj = NULL, seed = NULL, window.size = 50000) {
+  
+  if (is.null(trigger_obj)) {
+    trigger_obj <- trigger::trigger.build(marker = GRN$L, exp = GRN$Trait, 
+                                 marker.pos = GRN$L.pos, exp.pos = GRN$T.pos)
+    
+    utils::capture.output(
+      trigger_obj <- trigger::trigger.loclink(trigger_obj, window.size = window.size)
+    )
+  }
+  
+  tryCatch({
+    oldwd <- setwd("~/")
+    t(trigger::trigger.net(trigger_obj, seed = seed))
+  }, error = function(e) {
+    print(e)
+    NULL
+  }, finally = {
+    # Clean up file produced when running trigger.net
+    suppressWarnings(file.remove('~/net_trigg_prob.txt'))
+    setwd(oldwd)
+  })
 }
